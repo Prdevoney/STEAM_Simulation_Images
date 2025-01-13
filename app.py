dfd2186fe676fc461e29d1f7711f2ac1cc19988d
Prdevoney <prdevo@gmail.com>
2025-01-13 17:59:17 -0500

    Test server set up with multiple routes

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return jsonify({'message':'server is runningg'})
+
+@app.route("/test")
+def test():
+    return jsonify({'message':'addition is done'})
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=5000, debug=True)
