aec31fc343e5c31c3f7a06610df943a72acbaa5c
Prdevoney <prdevo@gmail.com>
2025-03-26 12:01:42 -0400

    added source to test_world_image and updated the gazebo run command so that the simulation starts in running mode.

--- test_world_image/entrypoint.sh
+++ test_world_image/entrypoint.sh
@@ -3,13 +3,14 @@ set -e
 
 # Step 1: Start start simulation
 cd gaz_worlds_files/
-ign gazebo -v 4 turtlebot3_baylands.sdf -s &
+ign gazebo -r turtlebot3_baylands.sdf -s &
 
 # Step 2: Launch Gazebos websocket server
 cd ..
 ign launch -v 4 /usr/share/ignition/ignition-launch5/configs/websocket.ign &
 
 # Step 3: Launch ROS2 bridge (different for each simulation) 
+source /opt/ros/humble/setup.bash
 ros2 run ros_ign_bridge parameter_bridge /model/turtlebot3_burger/cmd_vel@geometry_msgs/msg/Twist@ignition.msgs.Twist &
 		
 # Step 4: CLI web socket server 
