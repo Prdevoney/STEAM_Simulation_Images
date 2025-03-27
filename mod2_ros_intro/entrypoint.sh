32ea7733102d8def94b8ba31491204467d0977f4
Prdevoney <prdevo@gmail.com>
2025-03-26 22:45:02 -0400

    Set up environment for module two. Time to test it.

--- mod2_ros_intro/entrypoint.sh
+++ mod2_ros_intro/entrypoint.sh
@@ -2,16 +2,20 @@
 set -e
 
 # Step 1: Start start simulation
-cd gaz_worlds_files/
-ign gazebo -v 4 turtlebot3_baylands.sdf -s &
+ign gazebo -v 4 -r /usr/share/ignition/ignition-gazebo6/worlds/linear_battery_state.sdf -s &
 
 # Step 2: Launch Gazebos websocket server
-cd ..
 ign launch -v 4 /usr/share/ignition/ignition-launch5/configs/websocket.ign &
 
 # Step 3: Launch ROS2 bridge (different for each simulation) 
-ros2 run ros_ign_bridge parameter_bridge /model/turtlebot3_burger/cmd_vel@geometry_msgs/msg/Twist@ignition.msgs.Twist &
+# 3a: 
+source /opt/ros/humble/setup.bash
+ros2 run ros_ign_bridge parameter_bridge /model/vehicle_blue/battery/linear_battery/state@sensor_msgs/msg/BatteryState@ignition.msgs.BatteryState  &
 		
+# 3b:
+source /opt/ros/humble/setup.bash
+ros2 run ros_ign_bridge parameter_bridge /model/vehicle_blue/battery/linear_battery/state@sensor_msgs/msg/BatteryState@ignition.msgs.BatteryState &
+
 # Step 4: CLI web socket server 
 cd steam-websocket 
 npm start &
