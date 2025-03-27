f9359487ed1c356f5ef2fd7821ca5e3b149df1e5
Prdevoney <prdevo@gmail.com>
2025-03-27 11:44:15 -0400

    finished setting up the environment for mod5_drone.

--- mod5_drone/entrypoint.sh
+++ mod5_drone/entrypoint.sh
@@ -2,20 +2,20 @@
 set -e
 
 # Step 1: Start start simulation
-ign gazebo -v 4 -r /usr/share/ignition/ignition-gazebo6/worlds/multicoper_velocity_control.sdf -s &
+ign gazebo -v 4 -r /usr/share/ignition/ignition-gazebo6/worlds/multicopter_velocity_control.sdf -s &
 
 # Step 2: Launch Gazebos websocket server
 ign launch -v 4 /usr/share/ignition/ignition-launch5/configs/websocket.ign &
 
 # Step 3: Launch ROS2 bridge (different for each simulation) 
 # 3a: 
-soure /opt/ros/humble/setup.bash
+source /opt/ros/humble/setup.bash
 ros2 run ros_ign_bridge parameter_bridge /X4/gazebo/command/twist@geometry_msgs/msg/Twist@ignition.msgs.Twist  &	
 # 3b:
-soure /opt/ros/humble/setup.bash
+source /opt/ros/humble/setup.bash
 ros2 run ros_ign_bridge parameter_bridge /X3/gazebo/command/twist@geometry_msgs/msg/Twist@ignition.msgs.Twist &
 # 3c:
-soure /opt/ros/humble/setup.bash
+source /opt/ros/humble/setup.bash
 ros2 run ros_ign_bridge parameter_bridge /world/multicopter/model/X4/link/base_link/sensor/camera_front/camera_info@sensor_msgs/msg/CameraInfo@ignition.msgs.CameraInfo &
 
 # Step 4: CLI web socket server 
