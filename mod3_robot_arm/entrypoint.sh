57759eb4f5ee8eb0faca8bb54e082dcf86109e24
Prdevoney <prdevo@gmail.com>
2025-03-27 00:51:10 -0400

    completed the image for mod3. Some of the python scripts are getting errors. It may have just been the formatting. But one of them is working, so I am moving on to mod4. Then I'll do mod5 in the morning.

--- mod3_robot_arm/entrypoint.sh
+++ mod3_robot_arm/entrypoint.sh
@@ -2,16 +2,22 @@
 set -e
 
 # Step 1: Start start simulation
-cd gaz_worlds_files/
-ign gazebo -v 4 turtlebot3_baylands.sdf -s &
+ign gazebo -v 4 -r /usr/share/ignition/ignition-gazebo6/worlds/joint_trajectory_controller.sdf -s &
 
 # Step 2: Launch Gazebos websocket server
-cd ..
 ign launch -v 4 /usr/share/ignition/ignition-launch5/configs/websocket.ign &
 
 # Step 3: Launch ROS2 bridge (different for each simulation) 
-ros2 run ros_ign_bridge parameter_bridge /model/turtlebot3_burger/cmd_vel@geometry_msgs/msg/Twist@ignition.msgs.Twist &
-		
+# 3a: Joint Trajectory Controller
+source /opt/ros/humble/setup.bash
+ros2 run ros_ign_bridge parameter_bridge /model/RR_position_control/joint_trajectory@trajectory_msgs/msg/JointTrajectory@ignition.msgs.JointTrajectory &
+# 3b: Velocity Controller
+source /opt/ros/humble/setup.bash
+ros2 run ros_ign_bridge parameter_bridge /model/RR_velocity_control/joint_trajectory@trajectory_msgs/msg/JointTrajectory@ignition.msgs.JointTrajectory & 
+# 3c: Effort Controller
+source /opt/ros/humble/setup.bash
+ros2 run ros_ign_bridge parameter_bridge /custom_topic_effort_control@trajectory_msgs/msg/JointTrajectory@ignition.msgs.JointTrajectory &
+
 # Step 4: CLI web socket server 
 cd steam-websocket 
 npm start &
