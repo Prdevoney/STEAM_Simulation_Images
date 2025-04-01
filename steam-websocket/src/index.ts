5b0f4baa92035e81283f078685ea91f7879c3c46
Prdevoney <prdevo@gmail.com>
2025-04-01 13:29:36 -0400

    finished configuring steam-websocket for simulation restart logic

--- steam-websocket/src/index.ts
+++ steam-websocket/src/index.ts
@@ -3,12 +3,25 @@ import * as pty from 'node-pty';
 import * as os from 'os';
 import * as path from 'path';
 import * as fs from 'fs';
+import { exec } from 'child_process';
 
 const wss = new WebSocket.Server({ port: 8002 });
 
 console.log('Started web socket server on port 8002'); 
 // Determine the shell of the os, even though we know the conatiner is going to be Ubuntu (bash) 
 const shell = os.platform() === 'win32' ? 'powershell.exe' : 'bash';
+
+// Start script for each potential simulation 
+const simulation_script_map: Record<string, string> = {
+  "test_world_image": "ign gazebo -r gaz_worlds_files/turtlebot3_baylands.sdf", // test_world_image
+  "1LjViNIEB14XNArQtwaP": "", // mod2_ros_intro
+  "neOI52gdX1HInFQgE8Mp": "", // mod3_robot_arm
+  "bWSwj8u9RfeRd69jDkQ1": "", // mod4_tugbot
+  "hfQiob6b3V4WwvgcHyTf":"", // mod5_drone
+};
+
+const sim_key = "simulation_key"; 
+const gen_key = "general_key";
 
 // Map to keep track of interactive terminal sessions 
 const terminals = new Map(); 
@@ -37,8 +50,27 @@ function executeScript (data: any, term: any) {
   // Make python script executable 
   fs.chmodSync(tempFile, '755');
 
+  // clear terminal of anything that may be running 
+  term.write("\x03\r");
+
   // Execute the python script
   term.write(`python3 ${tempFile}\r`);
+}
+
+function executeStartScript (data: any, term: any) {
+  const script = simulation_script_map[data.module_id]; 
+  if (data.command === "restart") {
+    if (terminals.has(gen_key)) {
+      const gen_term = terminals.get(gen_key);
+      gen_term.write("\x03\r");
+    }
+    term.write("\x03\r");
+    term.write(`${script}\r`);
+  }
+  else if (data.command === "start") {
+    term.write("\x03\r");
+    term.write(`${script}\r`);
+  }
 }
 
 function interactiveInput (data: any, term: any) {
@@ -64,50 +96,65 @@ wss.on('connection', (ws: WebSocket) => {
     try {
       console.log('Payload: %s', data);
       const message = JSON.parse(data.toString());
-
-      // Check if question_id is provided
-      if (!message.question_id) {
-        console.log('No question_id provided');
-      }
       
-      if (message.term_type === "gen_terminal") {
+      if (message.term_type === "simulation_terminal") {
         try {
-          // Check if python script is provided
-          if (!message.python_script) {
-            console.log('No python script provided');
-            return; 
+          if (!message.command){
+            console.log('No command provided');
+            return;
           }
-          // create new terminal 
-          const term = spawnTerm();
-          // Call function to execute the python script
-          executeScript(message, term);
-          // Set up listener for this terminal
-          termOutput(term, ws);
+          // The simulation has already been created 
+          if (message.command === "restart"){
+            // get the terminal where the simulation is running
+            const term = terminals.get(sim_key); 
+            // execute restart command
+            executeStartScript(message, term);
+          } else if (message.command === "start") {
+            if (!message.module_id){
+              console.log('No module_id provided');
+              return; 
+            }
+            // create new terminal 
+            const term = spawnTerm();
+            terminals.set(sim_key, term);
+            executeStartScript(message, term);
+          }
         
         } catch (e) {
           console.error('Error: %s', e);
         }
-      } else if (message.term_type === "interactive_terminal") {
+      } else if (message.term_type === "general_terminal") {
           try {
-            // First call for interactive_terminal, create new terminal and run script 
+            // First call for gen_terminal, create new terminal and run script 
             if (!message.interactive_input) {
               if (!message.python_script) {
                 console.log('No python script provided');
                 return; 
               }
-              // create new terminal 
-              const term = spawnTerm(); 
-
-              // Logic for saving terminal in map with the message.question_id as the key
-              terminals.set(message.question_id, term);
-
-              // Call function executeScript to run the recieved python script 
-              executeScript(message, term); 
-              // Call function to output the terminal output to the websocket
-              termOutput(term, ws);
-            
+              if (!terminals.has(gen_key)) {
+                // create new terminal 
+                const term = spawnTerm(); 
+                // Logic for saving terminal in map with the message.question_id as the key
+                terminals.set(gen_key, term);
+                // Call function executeScript to run the recieved python script 
+                executeScript(message, term); 
+                // Call function to output the terminal output to the websocket
+                termOutput(term, ws);
+              } else {
+                // If the terminal already exists, get it from the map
+                const term = terminals.get(gen_key);
+                // Call function executeScript to run the recieved python script 
+                executeScript(message, term); 
+                // Call function to output the terminal output to the websocket
+                termOutput(term, ws);
+              }
+             
             } else {
-              const term = terminals.get(message.question_id);
+              if (!terminals.has(gen_key)) {
+                console.log('No terminal found for interactive input');
+                return; 
+              }
+              const term = terminals.get(gen_key);
               interactiveInput(message, term);
             }
           } catch (e) {
